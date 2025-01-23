9c4b41cb1fc3ef26aa4ef04cce3dfb6fbc409d5c
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-01-23 11:37:35 +0900

    fix : app 글로벌스타일 정정

--- src/0_App/index.js
+++ src/0_App/index.js
@@ -1,18 +1,32 @@
-import Page from "../page";
+import Page from "../1_Page";
 import { BrowserRouter } from "react-router-dom";
-import GlobalStyle from "./style/GlobalStyle.js";
 import { PC, Mobile } from "./model/useMediaQuery.js";
+import { CookiesProvider } from "react-cookie";
+import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
+
+import GlobalStyle from "./style/GlobalStyle.js";
+import ResetStyle from "./style/resetStyle.js";
+import theme from "./style/theme.js";
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <PC>
-        <Page />
-      </PC>
-      <Mobile>
-        <Page />
-      </Mobile>
-    </BrowserRouter>
+    <CookiesProvider>
+      <RecoilRoot>
+        <BrowserRouter>
+          <GlobalStyle />
+          <ResetStyle />
+          <ThemeProvider theme={theme.defaultTheme}>
+            <PC>
+              <Page />
+            </PC>
+            <Mobile>
+              <Page />
+            </Mobile>
+          </ThemeProvider>
+        </BrowserRouter>
+      </RecoilRoot>
+    </CookiesProvider>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import ReactDOM from "react-dom/client";
-import App from "./app";
-import { CookiesProvider } from "react-cookie";
-import { RecoilRoot } from "recoil";
+import App from "./0_App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <CookiesProvider>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
-  </CookiesProvider>
-);
+root.render(<App />);
